0156c2f5d8bc656a17b21473e935f57eb6ced4bf
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-08-04 17:53:56 +0530

    Updated cli.py for basic interface
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- cli.py
+++ cli.py
@@ -1,28 +1,59 @@
 from __future__ import print_function, unicode_literals
-from PyInquirer import prompt, print_json, Separator
+from PyInquirer import prompt
 from pprint import pprint
+import os
+
+print("\tWelcome to StenoCLI\n")
+
+questions = [
+    {
+        'type': 'input',
+        'name': 'name',
+        'message': 'What is your name?',
+    }
+]
+
+while(True):
+    answers = prompt(questions)
+    if (len(answers['name']) != 0):
+        print("Hello ", answers['name'], "! ")
+        break
+    print("Please input your name!")
+
+name = answers['name']
 
 questions = [
     {
         'type': 'checkbox',
-        'qmark': '😃',
-        'message': 'Select Command',
+        'qmark': '🤔',
+        'message': 'What do you wanna do ' + name + "?",
         'name': 'command',
         'choices': [
             {
-                'name': 'Photo Stenography'
+                'name': 'Photo-steganography'
             },
             {
-                'name': 'Text Stenography'
+                'name': 'Text-steganography'
             },
             {
                 'name': 'Exit'
             },
         ],
-        'validate': lambda answer: 'Choose one command to proceed!' \
-            if len(answer) == 0 else True
     }
 ]
 
-answers = prompt(questions)
-pprint(answers)
+while (True):
+    answers = prompt(questions)
+    if(len(answers['command']) != 0):
+        print('Ready to perform ', answers['command'][0])
+        break
+    print("Please select at least one option!")
+
+command = answers['command'][0]
+
+if command == 'Photo-steganography':
+    print('\tPhoto steganography\n')
+elif command == 'Text-steganography':
+    print('Text steganography Ready')
+else:
+    print('Exiting Program')
